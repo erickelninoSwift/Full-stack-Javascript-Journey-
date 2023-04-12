bcf31c73da7646cfb80380f27317f50676536a28
Erick El nino <erick.tshimbombo@yahoo.com>
2023-04-12 17:41:58 +0200

    Update message

--- Javascript Operators/main.js
+++ Javascript Operators/main.js
@@ -50,3 +50,66 @@ money = 100;
 console.log(money);
 
 
+let ericknumber = 0;
+
+if(ericknumber > 0)
+{
+    console.log(`This is a positive number ${ericknumber}`);
+}else
+{
+    console.log(`This is a negative ${ericknumber}`);
+}
+
+
+ericknumber > 0 ? console.log("The number is greater") : console.log("The number is not greater than 0");
+
+let color = "blue";
+
+switch(color)
+{
+    case "red":
+
+        console.log("Ist red");
+
+    break;
+
+    case "blue":
+
+            console.log("The color is blue");
+    break;
+}
+
+let num1 = 1;
+let num3 = 0;
+
+let maxValue;
+
+maxValue =  (num1 + num3) > 100 ? num1 + num3 : 300;
+
+console.log(maxValue);
+
+let number = 50;
+
+if(number % 5 === 0)
+{
+    console.log(`Fizz`);
+}else
+{
+    console.log(`Number is not ${number}`);
+}
+
+if(number % 4 === 0 && number % 5 === 0)
+{
+    console.log(`Number is : ${number}`);
+}else
+{
+    console.log('You see me as pass around');
+}
+
+
+(number % 5 === 0 && number % 10 === 0) ? console.log(`Number is Good`) : console.log(`Money is not everything`);
+
+(number % 10 === 0) ? console.log(`How are you doing ?`) : console.log(`Eriik was good`);
+
+
+
