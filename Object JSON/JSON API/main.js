5695e3729df2c7a73ace16650232db52abddf703
Erick El nino <erick.tshimbombo@yahoo.com>
2023-04-17 01:54:33 +0200

    JSON course

--- Object JSON/JSON API/main.js
+++ Object JSON/JSON API/main.js
@@ -22,8 +22,40 @@ let companies = `[
     }
 ]`
 
-console.log(JSON.parse(companies)[0].name);
+
+let schoolManagement = [
+    {
+        "name" : "big corporate ",
+        "numberOfEmployee" : 10000,
+        "ceo": "Mary",
+        "rating" : 3.8 ,
+        "isProgrammer" : true,
+        "skills" : ["html","Javascript","Css"]
+    },
+    {
+        "name" : "Facebook ",
+        "numberOfEmployee" : 1005600,
+        "ceo": "Mark",
+        "rating" : 4.8,
+        "isProgrammer" : true,
+        "skills" : ["Angular","Nodejs","MongoDB"]
+    },
+    {
+        "name" : "Netflix",
+        "numberOfEmployee" : 80000,
+        "ceo": null,
+        "rating" : 4.4,
+        "isProgrammer" : false,
+        "skills" : ["php","react","laravel"]
+    }
+]
+
+
+// console.log(JSON.parse(companies)[0].name);
 
 // companies.forEach(company =>{
 //     console.log(company);
 // });
+
+
+console.log(`The new App API ${schoolManagement[0].numberOfEmployee}`);
