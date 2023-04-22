8c7d28118f9a646048d0c7f791d00128a380bd46
Erick El nino <erick.tshimbombo@yahoo.com>
2023-04-23 00:57:56 +0200

    update

--- Javascript Dipesh Course/index.js
+++ Javascript Dipesh Course/index.js
@@ -1,1 +1,40 @@
 console.log('hello world');
+
+// const name = prompt("what is your name ?");
+// console.error(name);
+// console.warn("Hello jackpot here");
+
+// if(true)
+// {
+//     var number = 20;
+//     let numnber3 = 60;
+//     console.log(numnber3);
+//     console.log(number);
+// }
+
+// Operator
+
+let number = 1;
+
+for(let i = 0;i <= 10 ; i++)
+{
+    number++;
+    console.log(i);
+}   
+
+console.log(`total sum is : ${number}`);
+
+
+// String
+
+let firstname = "Erick";
+let surmane = "Elnino";
+let language = "French";
+
+let name = new String("How are you doing today?");
+
+console.log(name);
+console.log(firstname);
+console.log(surmane);
+
+console.log(typeof name);
