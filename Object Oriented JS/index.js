8771c1d394e80bffae71e3ca586b2f171e0e9e59
Erick El nino <erick.tshimbombo@yahoo.com>
2023-04-24 08:30:53 +0200

    update

--- Object Oriented JS/index.js
+++ Object Oriented JS/index.js
@@ -24,3 +24,20 @@ const user1 = getuserData("Eriik",89);
 const user2 = getuserData("Yollande",65);
 
 console.log(user1,user2);
+
+
+function electronics(name,price)
+{
+    this.name = name;
+    this.price = price;
+
+    this.walk = function()
+    {
+        return `The name of the product: ${this.name} and Cost R${this.price}`;
+    }
+
+    console.log(this);
+}
+
+const televisiion = new electronics("Television",1000);
+
