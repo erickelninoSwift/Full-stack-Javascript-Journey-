0153bf9c4850ec89bccc04d94b8ec5b8e0ee6f1e
Erick El nino <erick.tshimbombo@yahoo.com>
2023-04-24 07:05:37 +0200

    factory function

---
+++ Object Oriented JS/index.js
@@ -0,0 +1,32 @@
+console.log("Hello welcome to OOP in Javascript");
+
+const user = {
+    name: 'eriik',
+    age: 40,
+    walk: function()
+    {
+        console.log("i am starting to walk")
+    },
+}
+
+
+const user1 = {
+    name: 'jackpot',
+    age: 56,
+    swim: function()
+    {
+        console.log("i am starting to swim")
+    },
+}
+
+
+const user2 = {
+    name: 'elnino',
+    age: 100,
+    dance: function()
+    {
+        console.log("i am starting to Dance")
+    },
+}
+
+console.log(user,user1,user2)
--- RegEx/index.js
+++ RegEx/index.js
@@ -6,3 +6,36 @@ let regEx = /erick+@/i;
 console.log(regEx.test("I am erick@yahoo.com elnino i love playing Xbox one :)"));
 console.log(regEx.source);
 console.log(regEx.exec('how are you doing erick@'));
+
+let str = "erick";
+
+console.log(str.match(regEx));
+
+if((str.match(regEx)) !== null)
+{
+    console.log('hello world');
+}else
+{
+    console.log("jackpot here");
+}
+
+let RegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const email = prompt("Please enter your email Address : ");
+
+console.log(email.search(RegEx));
+
+if(email && email.search(RegEx) == 0)
+{
+    if(email.match(RegEx))
+    {
+        console.log("Email regular expression was respected");
+    }else
+    {
+        console.log("Please there has been an error  while trying to get the correct email addres for the field ")
+    }
+
+}else
+{
+    console.log("Please do not leave the field empty :)");
+}
