3c513de77ee155de2e07406e8b4763bb524f11b4
Erick El nino <erick.tshimbombo@yahoo.com>
2023-04-24 23:09:24 +0200

    static methode

--- CLASS/index.js
+++ CLASS/index.js
@@ -61,4 +61,29 @@ currentSquare.area = 15;
 console.log(`current element :${currentSquare.currentWidth}`);
 
 
+//  Static methode Class
+
+
+class triangle
+{
+    constructor(_with)
+    {
+        this.width = _with;
+        this.height = _with;
+    }
+
+    square( _width)
+    {
+        return _width * _width;
+    }
+
+}
+
+const newForm = new triangle(45);
+// console.log(`Number total : ${triangle.square(45)}`);
+
+// console.log(`The new form data total :${triangle.square(20)}`);
+
+console.log(`The total is : ${newForm.square(78)}`);
+
 
