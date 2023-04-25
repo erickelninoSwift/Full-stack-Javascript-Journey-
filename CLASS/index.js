3830aa48cf3e173371c8be7be093e81cd8365cad
Erick El nino <erick.tshimbombo@yahoo.com>
2023-04-25 18:12:46 +0200

    update

--- CLASS/index.js
+++ CLASS/index.js
@@ -87,3 +87,51 @@ const newForm = new triangle(45);
 console.log(`The total is : ${newForm.square(78)}`);
 
 
+//  Classs inheritance
+
+
+class Person 
+{
+    constructor(_name, _surname, _age)
+    {
+        this.name = _name;
+        this.surname = _surname;
+        this.age = _age;
+
+    }
+
+    greeting()
+    {
+        return ` Hello ${this.name} i heard you ${this.age} years old`;
+    }
+}
+
+const Erick = new Person('Eriik','Elnino', 31);
+
+console.log(Erick);
+
+
+class Student extends Person
+{
+
+    constructor(_name, _surname, _age, _course,_Money)
+    {
+        super(_name, _surname, _age);
+        this.course = _course;
+        this.Money = _Money;
+    }
+
+    code()
+    {
+        console.log(`My name is ${this.name}`);
+    }
+}
+
+
+const student1 = new Student("Yollande","Kasonga",60,"BioTech",12000);
+
+console.log(student1);
+student1.code();
+
+
+
