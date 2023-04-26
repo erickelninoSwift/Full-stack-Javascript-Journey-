f479672eff2d806949919a0a361a71e7b8c95668
Erick El nino <erick.tshimbombo@yahoo.com>
2023-04-26 18:42:20 +0200

    update

--- Asynchornous javascript/index.js
+++ Asynchornous javascript/index.js
@@ -1,29 +1,50 @@
-console.log("Today we are going to learn Async");
+// console.log("Today we are going to learn Async");
 
-setTimeout(() =>{
-    console.log("Hey how are you doing ?")
-},2000);
+// setTimeout(() =>{
+//     console.log("Hey how are you doing ?")
+// },2000);
 
-setTimeout(() => {
-    console.log("I am doing very well thank you ");
-}, 4000);
+// setTimeout(() => {
+//     console.log("I am doing very well thank you ");
+// }, 4000);
+
+// fetchUser(122231454,function(userElnino){
+//     console.log(userElnino)
+// })
 
 
-function fetchUser()
+// function fetchUser(userID,myrequest)
+// {
+//     setTimeout(() =>{
+//         console.log("hey this is jackpot here !");
+
+//         const fetchedUser = 
+//         {
+//             name: 'elnino',
+//             surname: 'cholo',
+//             age: 25
+//         }
+
+//         myrequest(fetchedUser);
+//     },3000);
+// }
+ 
+
+
+function sport(passport,callback)
 {
-    setTimeout(() =>{
-        return {
-            name:'erick',
-            age: 45,
-        }
-    },3000);
+    setTimeout(() => {
 
-    return{
-        name:'elnino',
-        age: 45
-    }
+        const nameofUser = `Userid : ${passport} can travel to any country`; 
+        
+        callback(nameofUser);
+    },3000);
 }
 
-const user = fetchUser();
 
-console.log(user);
+sport(213070797,(mysentence) =>{
+
+
+    console.log(mysentence);
+})
+
