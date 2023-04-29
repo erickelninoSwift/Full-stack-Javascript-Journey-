6d9ee96511b551895ebdcb20e84d038e1e2cba6a
Erick El nino <erick.tshimbombo@yahoo.com>
2023-04-29 03:53:20 +0200

    update

--- AJAX/index.js
+++ AJAX/index.js
@@ -1,24 +1,167 @@
-// console.log("Hello world today we gonna learn Ajax");
+// // console.log("Hello world today we gonna learn Ajax");
 
-function otherfunction()
+// function otherfunction()
+// {
+//     console.log("Hello my name is erick and i am here to learn ajax");
+// }
+
+// function MySecondMessage()
+// {
+//     console.log("we love you so much !!!");
+// }
+
+// setTimeout(() =>{
+
+//     otherfunction();
+
+// },2000);
+
+// setTimeout(() =>{
+
+//     MySecondMessage();
+
+// },4000);
+
+
+// setTimeout(() =>{
+
+//     console.log("Hwo are you doing today ?");
+
+//     setTimeout(() =>{
+
+//         console.log("I am doing well");
+
+//     },3000);
+
+
+// },6000);
+
+
+// const items = [1,2,3,4,5];
+
+// items.forEach(number =>{
+
+//     setTimeout(() =>{
+
+//         console.log(number);
+
+//     },4000);
+// });
+
+// function loginUser(email,password,callback)
+// {
+//     setTimeout(() =>{
+
+//         console.log(`The email is : ${email} and password : ${password}`);
+
+//        callback(() =>{
+
+//         return 'hello my name is eriik';
+
+//        });
+
+//     },5000);
+// }
+
+
+
+// const erick = loginUser("erick@yahoo.com",2130707979, mycallback =>{
+
+//     console.log(mycallback());
+// })
+
+
+// function getUserVideos(email,callback)
+// {
+//     setTimeout(() =>{
+
+//         callback(["Spider-mane","Pirate of Careabean","Alice in wondedrland"]);
+ 
+//     },9000);
+// }
+
+// const elnino = getUserVideos('erick@yahoo.com', videos => {
+
+//      console.log(`All the videos i have watched ${videos}`);
+
+// });
+
+console.log("Finish watching videos");
+
+
+
+let numberStudent = 50;
+
+const myPromise = new Promise((resolve,reject) =>{
+
+    setTimeout(() =>{
+
+        if(numberStudent >= 100)
+        {
+            resolve(`Number of student is good : ${numberStudent}`);
+        }else
+        {
+            reject(new Error('My freind this is an error'));
+        }
+
+    },3000);
+
+});
+
+myPromise.then(result =>{
+
+    console.log(result);
+
+})
+
+.catch(err =>{
+  
+    console.warn(`Error found : ${err}`);
+
+})
+
+.finally(() =>{
+
+    setTimeout(() =>{
+
+        console.log("The end.......");
+
+    },3000);
+});
+
+
+
+function getAlluser(userEmail,userpassword)
 {
-    console.log("Hello my name is erick and i am here to learn ajax");
+
+    return new Promise((resolve,reject) =>{
+
+        setTimeout(() =>{
+
+           resolve(`Username: ${userEmail} and password: ${userpassword}`);
+        },2000)
+    });
+
 }
 
-function MySecondMessage()
-{
-    console.log("we love you so much !!!");
-}
 
-setTimeout(() =>{
 
-    otherfunction();
 
-},2000);
+getAlluser('Elnino','Yalloande@1963').then(result =>{
 
-setTimeout(() =>{
+    
+    console.log(result);
 
-    MySecondMessage();
+}).catch(error =>{
 
-},4000);
+    console.warn("There was an error while trying to get the data");
+})
 
+.finally(() =>{
+
+    setTimeout(() => {
+        
+        console.log("This is great practice");
+
+    },3000);
+});
