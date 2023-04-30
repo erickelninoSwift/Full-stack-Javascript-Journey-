a2379d53af2bd82362214bdb6f54a4e63edddd17
Erick El nino <erick.tshimbombo@yahoo.com>
2023-04-30 03:12:51 +0200

    update data

--- Microblog/index.js
+++ Microblog/index.js
@@ -3,8 +3,28 @@
 
 console.log("Todo list");
 
-console.log(document.all.length);
-console.log(document.forms[0]);
+// console.log(document.all.length);
+// console.log(document.forms[0]);
+
+const div = document.querySelectorAll("div");
+const header = document.getElementsByClassName("header");
+
+const headers = Array.from(header);
+
+console.log(headers);
+
+let x = 0;
+
+headers.forEach(myheader =>{
+
+    myheader.style.color ="white";   
+    myheader.style.backgroundColor = "black";
+    myheader.style.borderRadius = "5px";
+    myheader.style.fontSize = "2rem"
+    myheader.style.padding = "10px";
+
+
+});
 
 
 
