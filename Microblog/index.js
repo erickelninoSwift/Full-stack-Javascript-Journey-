2034fa3d6ee404364c5118411eee370aa0ac1a06
Erick El nino <erick.tshimbombo@yahoo.com>
2023-04-30 02:29:10 +0200

    update dom manipulation

--- Microblog/index.js
+++ Microblog/index.js
@@ -1,2 +1,10 @@
 //  sessionStorage.setItem('name','Kazi');
 //  localStorage.setItem('name','Ariyan');
+
+console.log("Todo list");
+
+console.log(document.all.length);
+console.log(document.forms[0]);
+
+
+
