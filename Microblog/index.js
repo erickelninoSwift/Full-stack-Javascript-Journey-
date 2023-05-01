2edf947c5b84f58787d6d3804a99b020bc1616f8
Erick El nino <erick.tshimbombo@yahoo.com>
2023-05-01 02:03:39 +0200

    update

--- Microblog/index.js
+++ Microblog/index.js
@@ -6,28 +6,40 @@ console.log("Todo list");
 // console.log(document.all.length);
 // console.log(document.forms[0]);
 
-const div = document.querySelectorAll("div");
-const header = document.getElementsByClassName("header");
+// const div = document.querySelectorAll("div");
+// const header = document.getElementsByClassName("header");
 
-const headers = Array.from(header);
+// const headers = Array.from(header);
 
-console.log(headers);
+// console.log(headers);
 
-let x = 0;
+// let x = 0;
 
-headers.forEach(myheader =>{
+// headers.forEach(myheader =>{
 
-    myheader.style.color ="black";   
-    myheader.style.backgroundColor = "lightGray";
-    myheader.style.borderRadius = "2px";
-    myheader.style.fontSize = "2rem"
-    myheader.style.padding = "10px";
-});
+//     myheader.style.color ="black";   
+//     myheader.style.backgroundColor = "lightGray";
+//     myheader.style.borderRadius = "2px";
+//     myheader.style.fontSize = "2rem"
+//     myheader.style.padding = "10px";
+// });
 
-const h1 = document.getElementsByTagName("h1");
+// const h1 = document.getElementsByTagName("h1");
 
-console.log(h1);
+// const form = document.querySelector("#add-post-form");
 
+// form.style.backgroundColor = "white";
 
+// console.log(form);
 
+const tbody = document.querySelector("tbody");
+
+const allchild = tbody.children;
+
+allchild[2].style.color = "red";
+
+for(let index of allchild)
+{
+    index.innerHTML = "<p>How are you doing today ?</p>";
+}
 
