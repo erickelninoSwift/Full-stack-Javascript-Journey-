4ddef566f9ce3cb0b2f142d1ccb712ff69ac4cbc
Erick El nino <erick.tshimbombo@yahoo.com>
2023-05-02 02:47:34 +0200

    update data

--- Andrei negoie DOM/index.js
+++ Andrei negoie DOM/index.js
@@ -7,7 +7,7 @@ const list = document.querySelector("ul");
 
 const alllist = list.children;
 
-list.style.display = "flex";
+
 list.style.listStyle = "none";
 list.style.justifyContent = "center";
 
@@ -20,7 +20,11 @@ for(let index of alllist)
     index.style.height = "30px";
     index.style.textAlign = "center";
     index.style.width = "100px"
-    index.style.paddinTop = "20px"
+    index.style.borderRadius = "5px";
+    index.style.display = "block";
+    index.style.marginTop = "auto";
+
+    
     
 
     index.addEventListener('click', (e) =>{
@@ -36,9 +40,49 @@ const h1 = document.querySelector("main");
 
 const buttonclick = document.querySelector(".btn-danger");
 
-buttonclick.addEventListener('click',(e) =>{
+buttonclick.addEventListener('mouseenter',(e) =>{
     e.preventDefault = true;
     h1.style.color = "red";
 
     console.log("Click me!!!");
 });
+
+
+const buttonAdd = document.querySelector(".butto-add");
+const textData = document.querySelector(".text-input");
+const ul = document.querySelector("ul");
+const li = document.createElement("li");
+console.log(ul);
+
+buttonAdd.addEventListener('click',(e) =>{
+    e.preventDefault = true;
+    textData.value !== "" ? li.textContent = textData.value : console.log("No data");
+    ul.appendChild(li);
+
+    for(let index of alllist)
+{
+    index.style.backgroundColor = "blue";
+    index.style.color = "white";
+    index.style.margin = "2px";
+    index.style.fontSize = "10px"
+    index.style.height = "30px";
+    index.style.textAlign = "center";
+    index.style.width = "100px"
+    index.style.borderRadius = "5px";
+    index.style.display = "block";
+    index.style.marginTop = "auto";
+
+    
+    
+
+    index.addEventListener('click', (e) =>{
+
+        e.preventDefault = true;
+        console.log(index.textContent);
+    })
+}
+
+});
+
+
+
