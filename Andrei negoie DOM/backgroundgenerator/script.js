748d8a2ff272553a5de63ce7010201ecae84708d
Erick El nino <erick.tshimbombo@yahoo.com>
2023-05-04 13:23:19 +0200

    update

--- Andrei negoie DOM/backgroundgenerator/script.js
+++ Andrei negoie DOM/backgroundgenerator/script.js
@@ -1,1 +1,22 @@
 console.log("Gradient");
+
+
+const color1 = document.querySelector("#color1");
+const color2 = document.querySelector("#color2");
+
+// console.log(color1);
+// console.log(color2);
+
+color1.addEventListener('mouseover', (e) =>{
+
+	e.preventDefault = true;
+	console.log(e.target.value);
+})
+
+color2.addEventListener('mouseover', (e) =>{
+
+	e.preventDefault = true;
+	console.log(e.target.value);
+})
+
+
